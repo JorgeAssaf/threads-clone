766948500312c15455bfdf89ed5885b0e67a5cf5
Jorge Assaf <jorgeassaf160@gmail.com>
2024-01-19 20:58:52 -0600

    :wrench: Update ESLint, Next.js config, TypeScript config, Prettier config, and Tailwind CSS config

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    ecmaFeatures: {
+      jsx: true,
+    },
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'tailwindcss'],
+  extends: [
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'jsx-a11y/heading-has-content': 'off',
+  },
+  ignorePatterns: ['node_modules', '.next', '.eslintrc.cjs'],
+  settings: {
+    tailwindcss: {
+      callees: ['cn', 'cva'],
+      config: './tailwind.config.ts',
+      classRegex: '^(class(Name)?|tw)$',
+    },
+    next: {
+      rootDir: ['./'],
+    },
+  },
+}
+
+module.exports = config
