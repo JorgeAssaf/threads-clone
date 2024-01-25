aba22903d72bef743b75cbe822ba1dd58ca58eca
Jorge Assaf <jorgeassaf160@gmail.com>
2024-01-25 12:31:25 -0600

    :memo: Add zoom functionality to ZoomImage component

--- src/components/zoom-image.tsx
+++ src/components/zoom-image.tsx
@@ -1,8 +1,31 @@
 'use client'
 
+import { PlusIcon, X } from 'lucide-react'
 import MediumZoom from 'react-medium-image-zoom'
 
 import 'react-medium-image-zoom/dist/styles.css'
+
+
+// interface ZoomContentProps {
+//   img: React.ReactElement | null;
+//   buttonUnzoom: React.ReactElement<HTMLButtonElement>;
+//   onUnzoom: () => void;
+// }
+
+
+// const ZoomContent = ({ img, buttonUnzoom, onUnzoom }: ZoomContentProps) => {
+
+//   return (
+//     <div className='zoom-image'>
+//       <div className='zoom-image__image'>
+//         {img}
+//       </div>
+//       <div className='absolute top-0 right-0 z-10' onClick={onUnzoom}>
+//         <X />
+//       </div>
+//     </div>
+//   )
+// }
 
 export const ZoomImage = ({
   children,
@@ -10,12 +33,14 @@ export const ZoomImage = ({
   return (
     <MediumZoom
       wrapElement='div'
+      IconZoom={PlusIcon}
+      IconUnzoom={X}
       classDialog='zoom-image'
       zoomMargin={30}
       a11yNameButtonZoom='Zoom'
       a11yNameButtonUnzoom='Unzoom'
     >
       {children}
-    </MediumZoom>
+    </MediumZoom >
   )
 }
